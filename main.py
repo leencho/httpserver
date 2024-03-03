ce96f709bc5e6598c9b070078f228e8258f2a232
ahmed <ahmed>
2024-03-03 21:57:05 +0100

    basic GET request

--- main.py
+++ main.py
@@ -11,12 +11,6 @@ def file_write(filename, msg):
         file.write(msg)
 
 
-# def http_response(request):
-#    if request == "GET / HTTP/1.1":
-#        response = "HTTP/1.1 200 0k \r\n\n"
-#    return response
-
-
 class MyTCPRequestHandler(socketserver.StreamRequestHandler):
 
     def handle(self):
@@ -25,24 +19,35 @@ class MyTCPRequestHandler(socketserver.StreamRequestHandler):
         print(msg)
         file_content = file_read("index.html")
         file_length = len(file_content)
-        if msg.decode("utf-8") == "hello":
-            self.wfile.write("hello back to you\n".encode("utf-8"))
-        # elif msg == b"GET /index HTTP/1.1":
-        #    self.wfile.write(file_content.encode("utf-8"))
-        elif msg.decode("utf-8") == "GET / HTTP/1.1":
-            self.wfile.write("HTTP/1.1 200 OK \n\n\r".encode("utf-8"))
-        elif msg == b"GET /index HTTP/1.1":
-            print("correct!")
-            response = "HTTP/1.1 200 OK \r\n"
-            resp_len = len(response)
-            content_length = resp_len + file_length
-            response += "Content-Length: {}".format(file_length) + "\r\n"
-            response += "\r\n"
-            response += file_content
-            self.wfile.write(response.encode("utf-8"))
+
+        # handling get request
+        if msg.startswith(b"GET"):
+            if msg.decode("utf-8") == "GET / HTTP/1.1":
+                response = "HTTP/1.1 200 ok\r\n"
+                response += "Content-Type: text/html\r\n"
+                response += "Content-Length: {}".format(
+                    len(file_content)) + "\r\n"
+                response += "\r\n"
+                response += file_content
+                self.wfile.write(response.encode("utf-8"))
+            elif msg == b"GET /index HTTP/1.1":
+                response = "HTTP/1.1 200 OK \r\n"
+                resp_len = len(response)
+                content_length = resp_len + file_length
+                response += "Content-Length: {}".format(file_length) + "\r\n"
+                response += "\r\n"
+                response += file_content
+                self.wfile.write(response.encode("utf-8"))
+            else:
+                response = "HTTP/1.1 400 error\r\n"
+                response += "Content-Type: text/html\r\n"
+                response += "\r\n"
+                response += "<h1> 404 NOT FOUND </h>"
+                file_write("hello.txt", response)
+                self.wfile.write(response.encode("utf-8"))
         else:
-            file_write("hello.txt", msg.decode("utf-8"))
-            self.wfile.write("hello from server!\n".encode("utf-8"))
+            response = "HTTP/1.1 400 error\r\n"
+            self.wfile.write(response.encode("utf-8"))
 
 
 if __name__ == "__main__":
