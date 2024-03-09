01e21b1ccc0388488f32311f29dcbe20efcb6036
leencho <ahmed>
2024-03-09 20:56:12 +0100

    basic GET and POST request

--- main.py
+++ main.py
@@ -1,60 +1,72 @@
+#!/bin/env python3
+
+
 import socketserver
+import threading
+import signal
 
 
-def file_read(filename):
-    with open(filename, "r") as file:
-        return file.read()
-
-
-def file_write(filename, msg):
-    with open(filename, "w") as file:
-        file.write(msg)
-
-
-class MyTCPRequestHandler(socketserver.StreamRequestHandler):
+class MyTCPRequestHandler(socketserver.StreamRequestHandler, socketserver.ThreadingTCPServer):
 
     def handle(self):
         print("recieced request from {}".format(self.client_address[0]))
-        msg = self.rfile.readline().strip()
+        msg = self.rfile.readline().strip().split()
         print(msg)
-        file_content = file_read("index.html")
-        file_length = len(file_content)
-
-        # handling get request
-        if msg.startswith(b"GET"):
-            if msg.decode("utf-8") == "GET / HTTP/1.1":
-                response = "HTTP/1.1 200 ok\r\n"
-                response += "Content-Type: text/html\r\n"
-                response += "Content-Length: {}".format(
-                    len(file_content)) + "\r\n"
-                response += "\r\n"
-                response += file_content
-                self.wfile.write(response.encode("utf-8"))
-            elif msg == b"GET /index HTTP/1.1":
-                response = "HTTP/1.1 200 OK \r\n"
-                resp_len = len(response)
-                content_length = resp_len + file_length
-                response += "Content-Length: {}".format(file_length) + "\r\n"
-                response += "\r\n"
-                response += file_content
-                self.wfile.write(response.encode("utf-8"))
-            else:
-                response = "HTTP/1.1 400 error\r\n"
-                response += "Content-Type: text/html\r\n"
-                response += "\r\n"
-                response += "<h1> 404 NOT FOUND </h>"
-                file_write("hello.txt", response)
-                self.wfile.write(response.encode("utf-8"))
+        recv_data = {
+            "method": msg[0],
+            "path": msg[1],
+            "protocol": msg[2]
+        }
+        if recv_data["protocol"] == b'HTTP/1.1' or recv_data["protocol"] == b'HTTP/1.0':
+            while len(msg) != 0 and msg != b"\r\n":
+                msg = self.rfile.readline().split()
+                if len(msg) != 0:
+                    recv_data[msg[0].strip(b":")] = msg[1]
+            if recv_data["method"] == b"POST":
+                self.post_request(recv_data)
+            elif recv_data["method"] == b"GET":
+                self.get_Request(recv_data)
         else:
-            response = "HTTP/1.1 400 error\r\n"
-            self.wfile.write(response.encode("utf-8"))
+            response = b"505 HTTP Version Not Supported\r\n"
+            self.wfile.write(response)
+
+    def post_request(self, recv_data):
+        print("the protocol", recv_data["protocol"])
+        if recv_data["path"] == b"/" or recv_data["path"] == b"/test.txt":
+            data_length = int(recv_data[b'Content-Length'].decode("utf-8"))
+            message = self.rfile.read(data_length)
+            print(message)
+            with open("test.txt", "w") as file:
+                file.write(message.decode("utf-8"))
+            response = b"200 OK\r\n"
+            self.wfile.write(response)
+        else:
+            response = b"404 error\r\n"
+            self.wfile.write(response)
+
+    def get_Request(self, recv_data):
+        if recv_data["path"] == b"/" or recv_data["path"] == b"/index":
+            with open("index.html", "r") as file:
+                file_content = file.read()
+            content_len = len(file_content)
+            response = b"%b 200 OK\r\n" % recv_data['protocol']
+            response += b"Content-Type: text/html\r\n"
+            response += b"Content-Length: %d\r\n" % content_len
+            response += b"\r\n"
+            response += file_content.encode("utf-8")
+            self.wfile.write(response)
+        else:
+            response = b"404 error\r\n"
+            self.wfile.write(response)
 
 
 if __name__ == "__main__":
     HOST, PORT = "127.0.0.1", 9001
-    server = socketserver.TCPServer((HOST, PORT), MyTCPRequestHandler)
-    print("serving msg... on {}:{}".format(HOST, PORT))
-    try:
-        server.serve_forever()
-    except:
-        print("shutting down the")
+    with socketserver.TCPServer((HOST, PORT), MyTCPRequestHandler) as server:
+        try:
+            thread = threading.Thread(target=server.serve_forever)
+            print("connected to {}:{}".format(HOST, PORT))
+            thread.start()
+            thread.join()
+        except:
+            print("shutting down!")
